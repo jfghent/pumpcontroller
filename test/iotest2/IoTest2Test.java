8fdb9ddd9a2f72f1df0a7c2915cf204fb590ee3a
Jon <Jon@DESKTOP-2N32P02>
2018-08-25 11:54:36 -0500

    Created publishTopic as a sub method for all publishXXXX() methods.

---
+++ test/iotest2/IoTest2Test.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iotest2;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jon
+ */
+public class IoTest2Test {
+    
+    public IoTest2Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of main method, of class IoTest2.
+     */
+    @Test
+    public void testMain() throws Exception {
+        System.out.println("main");
+        String[] args = null;
+        IoTest2.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
